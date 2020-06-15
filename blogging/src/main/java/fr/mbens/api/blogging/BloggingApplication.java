7bd3208cc85e43f6ee73cd52178d3acd07dff61c
Mehdi Bennis <mehdi.bennis01@etu.umontpellier.fr>
2020-06-15 10:31:01 +0200

    first commit

---
+++ blogging/src/main/java/fr/mbens/api/blogging/BloggingApplication.java
@@ -0,0 +1,13 @@
+package fr.mbens.api.blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplication.class, args);
+	}
+
+}
---
+++ blogging/src/test/java/fr/mbens/api/blogging/BloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mbens.api.blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
